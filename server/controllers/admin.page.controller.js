78b6b3b67efa6a2af36f016ece8fbded619271b6
Subhamay Debnath <subhamaydebnath@gmail.com>
2024-11-03 20:35:01 +0530

    "Updated admin page controller, routes, and views: added new pages, refactored code, and made minor styling changes."

--- server/controllers/admin.page.controller.js
+++ server/controllers/admin.page.controller.js
@@ -1,44 +1,83 @@
 const utilsLayout = "../views/layouts/utils";
-const adminLayout = '../views/layouts/admin'
-const dashboard=async (req,res,next) => {
-    try {
-        const locals = {
-            title: "Dashboard",
-            description: "Welcome to Dashboard",
-        };
-        res.render('admin/index',{ locals,layout: adminLayout,})
-    } catch (error) {
-        console.log(`Dashboard error : ${error}`);
-        res.render('utils/error',{ locals:{title: "Error",description: "Welcome to our home page"}, layout: utilsLayout,message:error })
-    }
-}
-const articlesPage=async (req,res,next) => {
-    try {
-        const locals = {
-            title: "Article",
-            description: "Welcome to Article",
-        };
-        res.render('admin/articles',{ locals,layout: adminLayout,})
-    } catch (error) {
-        console.log(`Dashboard error : ${error}`);
-        res.render('utils/error',{ locals:{title: "Error",description: "Welcome to our home page"}, layout: utilsLayout,message:error })
-    }
-}
-const categoriesPage=async (req,res,next) => {
-    try {
-        const locals = {
-            title: "Categories",
-            description: "Welcome to Categories",
-        };
-        res.render('admin/categories',{ locals,layout: adminLayout,})
-    } catch (error) {
-        console.log(`Categories error : ${error}`);
-        res.render('utils/error',{ locals:{title: "Error",description: "Welcome to our home page"}, layout: utilsLayout,message:error })
-    }
-}
-
-export{
-    dashboard,
-    articlesPage,
-    categoriesPage
-}
+const adminLayout = "../views/layouts/admin";
+const dashboard = async (req, res, next) => {
+  try {
+    const locals = {
+      title: "Dashboard",
+      description: "Welcome to Dashboard",
+    };
+    res.render("admin/index", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(`Dashboard error : ${error}`);
+    res.render("utils/error", {
+      locals: { title: "Error", description: "Welcome to our home page" },
+      layout: utilsLayout,
+      message: error,
+    });
+  }
+};
+const articlesPage = async (req, res, next) => {
+  try {
+    const locals = {
+      title: "Article",
+      description: "Welcome to Article",
+    };
+    res.render("admin/articles", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(`Dashboard error : ${error}`);
+    res.render("utils/error", {
+      locals: { title: "Error", description: "Welcome to our home page" },
+      layout: utilsLayout,
+      message: error,
+    });
+  }
+};
+const categoriesPage = async (req, res, next) => {
+  try {
+    const locals = {
+      title: "Categories",
+      description: "Welcome to Categories",
+    };
+    res.render("admin/categories", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(`Categories error : ${error}`);
+    res.render("utils/error", {
+      locals: { title: "Error", description: "Welcome to our home page" },
+      layout: utilsLayout,
+      message: error,
+    });
+  }
+};
+const usersPage = async (req, res, next) => {
+  try {
+    const locals = {
+      title: "Users",
+      description: "Welcome to Users",
+    };
+    res.render("admin/users", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(`Users error : ${error}`);
+    res.render("utils/error", {
+      locals: { title: "Error", description: "Welcome to our home page" },
+      layout: utilsLayout,
+      message: error,
+    });
+  }
+};
+const contactPage = async (req, res, next) => {
+  try {
+    const locals = {
+      title: "Contact Form",
+      description: "Welcome to Contact Form",
+    };
+    res.render("admin/contact", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(`Contact Form error : ${error}`);
+    res.render("utils/error", {
+      locals: { title: "Error", description: "Welcome to our home page" },
+      layout: utilsLayout,
+      message: error,
+    });
+  }
+};
+export { dashboard, articlesPage, categoriesPage, usersPage, contactPage };
--- server/routes/admin.page.routes.js
+++ server/routes/admin.page.routes.js
@@ -3,9 +3,13 @@ import {
   dashboard,
   articlesPage,
   categoriesPage,
+  usersPage,
+  contactPage,
 } from "../controllers/admin.page.controller.js";
 const router = express.Router();
 router.get("/", dashboard);
 router.get("/articles", articlesPage);
 router.get("/categories", categoriesPage);
+router.get("/users", usersPage);
+router.get("/contact", contactPage);
 export default router;
