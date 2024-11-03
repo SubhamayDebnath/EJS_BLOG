c574b8a86faa11edc3ea8f6c30efc3dc54a359a2
Subhamay Debnath <subhamaydebnath@gmail.com>
2024-11-03 19:34:41 +0530

    "Added categoriesPage to main.page.controller.js import and created a new route for it"

--- server/routes/main.page.routes.js
+++ server/routes/main.page.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { homePage ,articlesPage,contactPage} from "../controllers/main.page.controller.js";
+import { homePage ,articlesPage,contactPage, categoriesPage} from "../controllers/main.page.controller.js";
 const router = express.Router();
 
 router.get("/", homePage);
 router.get('/articles',articlesPage);
-// router.get('/post/:id',fullPostPage)
+router.get('/categories', categoriesPage)
 router.get('/contact',contactPage);
 // router.get('/posts',allPostPage);
 
